c059b773786330265bb43cb4dae78b6da0e244c7
cloudpunchx <siobhanbonardi@hotmail.com>
2023-03-11 00:32:22 -0700

    Implemented User Watchlist GET request, tested with postman, working as expected.

--- endpoints/movie.py
+++ endpoints/movie.py
@@ -44,3 +44,27 @@ def get_featured_movies():
         return make_response(jsonify(response), 200)
     else:
         return make_response(jsonify('Something went wrong, please try again.'), 500)
+
+#GET User's Watchlist
+@app.get('/api/user-watchlist')
+def get_user_watchlist():
+    """
+    Required:
+    Token
+    """
+    required_data = ['token']
+    check_result = check_data(request.json, required_data)
+    if check_result != None:
+        return check_result
+    token = request.json.get('token')
+    keys = ['ID','MovieName', 'Certification', 'Release_Date', 'Genres', 'Language', 'Budget', 'Revenue', 'Runtime', 'poster']
+    result = run_statement('CALL get_user_watchlist(?)', [token])
+    response = []
+    if(type(result) == list):
+        if result == []:
+            return make_response(jsonify('Something went wrong, please try again.'), 500)
+        for movie in result:
+            response.append(dict(zip(keys, movie)))
+        return make_response(jsonify(response), 200)
+    else:
+        return make_response(jsonify('Something went wrong, please try again.'), 500)
