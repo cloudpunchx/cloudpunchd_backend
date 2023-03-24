fbaa273cefdbd3de88988ba9e2854cc754b4fd77
cloudpunchx <siobhanbonardi@hotmail.com>
2023-03-23 21:16:38 -0600

    Final commit before handing in MVP. (not complete, will finish after deadline)

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ from app import app
 from flask_cors import CORS
 from dbcreds import production_mode
 
+# using port 5001 because Foodie2 is 5000
 if (production_mode == True):
     print("Running server in Production Mode")
     import bjoern #type:ignore
-    bjoern.run(app, "0.0.0.0", 5000)
+    bjoern.run(app, "0.0.0.0", 5001)
 else:
     print("Running Server in Testing Mode.")
     from flask_cors import CORS
